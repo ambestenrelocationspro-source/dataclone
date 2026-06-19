2244ead14da71311f6b177ddec17b718a4212cb1
ambestenrelocationspro-source <ambestenrelocationspro@gmail.com>
2026-06-19 15:36:00 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const shopifyDomain = "hostingfile72.jobchayan.com";
+  const shopifyDomain = "jobsnearme.jobjaniye.com";
   const proxyHost = req.headers.host;
 
   const targetURL = `https://${shopifyDomain}${req.url}`;
